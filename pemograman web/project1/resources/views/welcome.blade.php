a62ba06a3215d1cd359ee752af0e2441989a038d
adelia300304 <adeliaadel7811@gmail.com>
2024-09-14 13:05:19 +0700

    second commit, add home, login, register

---
+++ pemograman web/project1/resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+
+//DART FUNDAMENTAL #1
+//AVRILLA RIZKI
+
+void main() {
+    string nama = 'Rilla';
+    string kota = 'Padang';
+
+    print('hello saya avrilla dan biasanya orang memanggil $nama. saya tinggal di $kota.');
+}
